7da147f3be357e12da84be455cfb6724974bc7f5
bakitova2006-collab <bakitova2006@gmail.com>
2026-03-04 16:56:42 +0500

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,1 +1,20 @@
+// firebase-init.js
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "ТВОЙ_API_KEY",
+  authDomain: "komek-code-course.firebaseapp.com",
+  projectId: "komek-code-course",
+  storageBucket: "komek-code-course.appspot.com",
+  messagingSenderId: "254136931241",
+  appId: "1:254136931241:web:5cecafb7235e7cd5f7eab8"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// 🔹 вот что было нужно
+export const auth = getAuth(app);
+export const db = getFirestore(app);
