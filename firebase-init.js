0e6eb901e2a8e651fedba87fbd63d918132fb0cc
bakitova2006-collab <bakitova2006@gmail.com>
2026-03-04 19:43:24 +0500

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,4 +1,9 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBFhrLF3OCYSOHRFSPROEP1qOvjopaK-8s",
   authDomain: "komek-code-course.firebaseapp.com",
@@ -8,3 +13,7 @@ const firebaseConfig = {
   appId: "1:254136931241:web:5cecafb7235e7cd5f7eab8",
   measurementId: "G-9BZPDQ66ZM"
 };
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
