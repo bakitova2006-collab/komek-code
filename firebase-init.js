07dbac7275fa1df10fcd4d728362c5167398b111
bakitova2006-collab <bakitova2006@gmail.com>
2026-03-04 19:40:34 +0500

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,20 +1,10 @@
-// firebase-init.js
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "ТВОЙ_API_KEY",
+  apiKey: "AIzaSyBFhrLF3OCYSOHRFSPROEP1qOvjopaK-8s",
   authDomain: "komek-code-course.firebaseapp.com",
   projectId: "komek-code-course",
-  storageBucket: "komek-code-course.appspot.com",
+  storageBucket: "komek-code-course.firebasestorage.app",
   messagingSenderId: "254136931241",
-  appId: "1:254136931241:web:5cecafb7235e7cd5f7eab8"
+  appId: "1:254136931241:web:5cecafb7235e7cd5f7eab8",
+  measurementId: "G-9BZPDQ66ZM"
 };
-
-const app = initializeApp(firebaseConfig);
-
-// 🔹 вот что было нужно
-export const auth = getAuth(app);
-export const db = getFirestore(app);
