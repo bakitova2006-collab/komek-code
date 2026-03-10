f4409fdccf862037de5cf06a2511a9e505bf17a5
bakitova2006-collab <bakitova2006@gmail.com>
2026-03-10 17:38:35 +0500

    Update student-home.js

--- student-home.js
+++ student-home.js
@@ -5,25 +5,17 @@ import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase
 const studentName = document.getElementById("studentName");
 
 onAuthStateChanged(auth, async (user) => {
+  if (!user) {
+    location.href = "auth.html?role=student";
+    return;
+  }
 
-if(!user){
-location.href = "auth.html?role=student";
-return;
-}
+  const snap = await getDoc(doc(db, "profiles", user.uid));
 
-const snap = await getDoc(doc(db,"profiles",user.uid));
-
-if(snap.exists()){
-
-const data = snap.data();
-
-studentName.textContent =
-`Привет, ${data.fullName}! Продолжай обучение.`;
-
-}else{
-
-studentName.textContent = user.email;
-
-}
-
+  if (snap.exists()) {
+    const data = snap.data();
+    studentName.textContent = `Привет, ${data.fullName}! Выбери модуль и продолжай обучение.`;
+  } else {
+    studentName.textContent = `Привет! Продолжай обучение.`;
+  }
 });
