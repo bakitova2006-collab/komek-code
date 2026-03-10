338db2b82726643a93ad711cc5938ef801b0394d
bakitova2006-collab <bakitova2006@gmail.com>
2026-03-10 14:38:14 +0500

    Create student-home.js

---
+++ student-home.js
@@ -0,0 +1,29 @@
+import { auth, db } from "./firebase-init.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const studentName = document.getElementById("studentName");
+
+onAuthStateChanged(auth, async (user) => {
+
+if(!user){
+location.href = "auth.html?role=student";
+return;
+}
+
+const snap = await getDoc(doc(db,"profiles",user.uid));
+
+if(snap.exists()){
+
+const data = snap.data();
+
+studentName.textContent =
+`Привет, ${data.fullName}! Продолжай обучение.`;
+
+}else{
+
+studentName.textContent = user.email;
+
+}
+
+});
